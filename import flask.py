b98bc06f7ed4861ecc5a7caae2be1a383bd5f365
OCHKOTUBERKULEZNOE <rignuliapuk182@mail.ru>
2022-12-26 21:06:02 +0400

    new flask

---
+++ import flask.py
@@ -0,0 +1,10 @@
+import flask
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+  return "hello,this is our first flask website ";
+
+if __name__=='__main__':
+    app.run(debug = True)
+#kek
